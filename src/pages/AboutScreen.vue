be457cd02841138045aa9bb394bda8249245c107
Ideiasweb <ideiasweb@MacBook-Pro-de-Ideiasweb.local>
2020-03-21 23:29:08 -0400

    tela sobre

--- src/pages/AboutScreen.vue
+++ src/pages/AboutScreen.vue
@@ -1,6 +1,15 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1>Aboute</h1>
+  <q-page style="padding:1em">
+    <q-card>
+      <q-card-section>
+        <div>
+          Aplicativo feito utilizando o Quasar-framework.
+        </div>
+        <div>
+          Para os dados utilizei a API: https://github.com/NovelCOVID/API .
+        </div>
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
